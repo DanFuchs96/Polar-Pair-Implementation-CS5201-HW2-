442a29de90c77aa6deac1719a4b8a5592073efcc
Daniel Fuchs <DanFuchs96@gmail.com>
2018-02-06 21:50:39 -0600

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/ninjahw2.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ninjahw2.dir/main.cpp.obj"
+  "ninjahw2.pdb"
+  "ninjahw2.exe"
+  "ninjahw2.exe.manifest"
+  "libninjahw2.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ninjahw2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
